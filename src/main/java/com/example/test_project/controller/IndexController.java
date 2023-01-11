d40eb7bfc3847763b661ddd60db7c2cb38e916bf
luck1306 <luck1306@naver.com>
2023-01-11 10:57:44 +0900

    20230111

--- src/main/java/com/example/test_project/controller/IndexController.java
+++ src/main/java/com/example/test_project/controller/IndexController.java
@@ -1,2 +1,18 @@
-package com.example.test_project.controller;public class IndexController {
+package com.example.test_project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping
+    public String root() {
+        return "root";
+    }
+
+    @GetMapping("/get")
+    public String get() {
+        return "get";
+    }
 }
